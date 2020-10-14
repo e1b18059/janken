d59b3c6ba7f4095ebbd419691972de7a0e31c3f3
e1b18059 <e1b18059@oit.ac.jp>
2020-10-14 12:28:05 +0900

    セキュリティの認証処理を実装する

--- src/main/java/oit/is/z0755/kaizi/janken/security/Lec03AuthConfiguration.java
+++ src/main/java/oit/is/z0755/kaizi/janken/security/Lec03AuthConfiguration.java
@@ -10,11 +10,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * JankenAuthConfiguration
+ * Lec03AuthConfiguration
  */
 @Configuration
 @EnableWebSecurity
-public class JankenAuthConfiguration extends WebSecurityConfigurerAdapter {
+public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter {
 
   /**
    * 誰がログインできるか(認証処理)
@@ -23,16 +23,14 @@ public class JankenAuthConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
     // $ sshrun htpasswd -nbBC 10 user1 pAssw0rd
-    /*
-     * auth.inMemoryAuthentication().withUser("user1")
-     * .password("$2y$10$rJ9yqGht2W96MdIJICRQQOuUiYrt2eDokKnDuZZof2DPs83PN6QdC").
-     * roles("USER"); auth.inMemoryAuthentication().withUser("admin")
-     * .password("$2y$10$rJ9yqGht2W96MdIJICRQQOuUiYrt2eDokKnDuZZof2DPs83PN6QdC").
-     * roles("ADMIN");
-     */
+    auth.inMemoryAuthentication().withUser("user1")
+        .password("$2y$10$oiXbdOuy3nnbRt//p5R6eOORuxeeQqw60NumGLTL1NcIN9uhmQp.i").roles("USER");
+    auth.inMemoryAuthentication().withUser("user2")
+        .password("$2y$10$tDVJWAqs7kFDDPo1jN43auuxZ.nIb1cmksQAfuWn8x7AVRpiju2dy").roles("USER");
+
     // 開発中は↓の書き方でも良いが，平文でパスワードが保存される
-    // auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("pAssw0rd")).roles("USER");
-    // auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("pAssw0rd")).roles("ADMIN");
+    // auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("password1")).roles("USER");
+    // auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder().encode("password2")).roles("USER");
   }
 
   @Bean
@@ -49,13 +47,13 @@ public class JankenAuthConfiguration extends WebSecurityConfigurerAdapter {
     // Spring Securityのフォームを利用してログインを行う
     http.formLogin();
 
-    // http://localhost:8000/sample3 で始まるURLへのアクセスはログインが必要
+    // http://localhost:8080/lec02 で始まるURLへのアクセスはログインが必要
     // antMatchers().authenticated がantMatchersへのアクセスに認証を行うことを示す
     // antMatchers()の他にanyRequest()と書くとあらゆるアクセス先を表現できる
     // authenticated()の代わりにpermitAll()と書くと認証処理が不要であることを示す
     http.authorizeRequests().antMatchers("/lec02/**").authenticated();
 
-    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8000/ に戻る
+    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8080/ に戻る
     http.logout().logoutSuccessUrl("/");
   }
 
