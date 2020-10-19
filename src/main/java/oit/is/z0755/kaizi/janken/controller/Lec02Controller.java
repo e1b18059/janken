0dfc0bcd60693424a872cf8c156ecbe5fe9f3bc5
e1b18059 <e1b18059@oit.ac.jp>
2020-10-19 17:59:17 +0900

    GETリクエストを実装する

--- src/main/java/oit/is/z0755/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0755/kaizi/janken/controller/Lec02Controller.java
@@ -1,5 +1,8 @@
 package oit.is.z0755.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,19 +10,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z0755.kaizi.janken.model.Janken;
+import oit.is.z0755.kaizi.janken.model.Entry;
 
 @Controller
 public class Lec02Controller {
 
-  /**
-   * lec02というGETリクエストがあったら，lec02()を呼び出して，lec02.htmlを返すメソッド
-   *
-   * @return
-   */
-  @GetMapping("/lec02")
-  public String lec02() {
-    return "lec02.html";
-  }
+  @Autowired
+  private Entry entry;
 
   @GetMapping("/lec02_1")
   public String lec02_1(@RequestParam Integer janken, ModelMap model) {
@@ -28,6 +25,15 @@ public class Lec02Controller {
     model.addAttribute("myhand", jk.myhand());
     model.addAttribute("yourhand", jk.yourhand());
     model.addAttribute("JankenResult", jk.judge());
+    return "lec02.html";
+  }
+
+  @GetMapping("/lec02")
+  public String lec02(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
+
     return "lec02.html";
   }
 
